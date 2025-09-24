cce5f67ef48360fdd5927b946e7acc2c2d54bf4c
psuti-slut <bebrabebra>
2025-09-24 23:53:52 +0400

    fourth task

---
+++ fourth/fourth.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	FirstOperand := 5
+	SecondOperand := 3
+	fmt.Println("FirstOperand, SecondOperand:", FirstOperand, SecondOperand)
+	fmt.Println("a + b =", FirstOperand + SecondOperand)
+	fmt.Println("a - b =", FirstOperand - SecondOperand)
+	fmt.Println("a * b =", FirstOperand * SecondOperand)
+	fmt.Println("a / b =", FirstOperand / SecondOperand)
+	fmt.Println("a % b =", FirstOperand % SecondOperand)
+}
