bc504145dd44ac9133c01b340ebfde89310de368
psuti-slut <bebrabebra>
2025-09-24 23:53:19 +0400

    second task

---
+++ second/second.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		SomeNumero int = 0
+		SomeFloat float64 = 1.314
+		Name string = "Johan"
+		isMonster = true
+	)
+
+	fmt.Println(SomeNumero, SomeFloat, Name, isMonster)
+}
